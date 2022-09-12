29eab4bdb05f08642d7497645ec840d21a6d21b1
benpeng <0083gp03@gmail.com>
2022-09-12 18:00:28 +0800

    add cart

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,21 @@
 import Link from 'next/link';
-import styles from '../styles/Navbar.module.css'
+import { useSelector } from 'react-redux';
+import styles from '../styles/Navbar.module.css';
+type item = {
+  id: number,
+  image:string,
+  product:string,
+  category:string,
+  price:number
+  quantity:number
+}
 const Navbar = () => {
+  const cart = useSelector((state:any) => state.cart);
+
+  const getItemsCount = () => {
+    return cart.reduce((accumulator:any, item:item) => accumulator + item.quantity, 0);
+  };
+
   return (
     <nav className={styles.navbar}>
       <h6 className={styles.logo}>GamesKart</h6>
@@ -12,7 +27,9 @@ const Navbar = () => {
           <Link href="/shop">Shop</Link>
         </li>
         <li className={styles.navlink}>
-          <Link href="/cart">Cart</Link>
+          <Link href="/cart">
+            <a>Cart ({getItemsCount()})</a>
+          </Link>
         </li>
       </ul>
     </nav>
