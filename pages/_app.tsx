0a291ce8b7d8680b702c7bf68970b6de3cf501ac
benpeng <0083gp03@gmail.com>
2022-09-12 17:44:10 +0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
+import { Provider } from 'react-redux';       // Importing Provider
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import '../styles/globals.css'
+import store from '../redux/store';           // Importing redux store
+import '../styles/globals.css';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <Provider store={store}>
     <div className="wrapper">
       <Navbar />
       <Component {...pageProps} />
       <Footer />
     </div>
+  </Provider>
   )
 }
 
